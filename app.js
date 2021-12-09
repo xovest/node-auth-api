eda4c1e10c9223885121f914efbe17c6c58c84e5
xovest <79008917+xovest@users.noreply.github.com>
2021-12-09 22:29:36 +0200

    sign and log in

--- app.js
+++ app.js
@@ -10,4 +10,25 @@ app.get('/api', (req, res) => {
   });
 });
 
+app.post('/api/posts', (req, res) => {
+  res.json({
+    message: 'post created dude'
+  });
+});
+
+app.post('/api/login', (req, res) => {
+  //creating a user
+  const user = {
+    id : 1,
+    username: 'ryan',
+    email: 'ryan@gmail.com',
+  };
+
+  jwt.sign({user}, 'secretkey', async (err, token) => {
+    await res.json({
+      token
+    });
+  });
+});
+
 app.listen(PORT, () => console.log(`Da serv iz runnin on port ${PORT}`));
